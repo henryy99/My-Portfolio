e1313358b9258a9921752f938b9df01701c9f3e9
dementoh <anhchangdemen019@gmail.com>
2025-09-18 01:24:33 -0400

    Fix some errors

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,7 @@
 function Navbar() {
   return (
     <nav className="w-full h-20 fixed z-10 bg-primary-dark shadow-xl border-b-1 border-b-dark-green hidden sm:block">
-      <ul className="flex justify-center gap-10 items-center h-full text-lg text-text-grey uppercase tracking-wider">
+      <ul className="flex justify-center gap-10 items-center h-full text-lg text-text-grey uppercase tracking-wider ">
         <li>
           <a href="#home">Home</a>
         </li>
